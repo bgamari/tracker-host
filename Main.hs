bd6d21ef4d9f88f045288880439afc506347d40e
Ben Gamari <bgamari.foss@gmail.com>
2013-08-04 16:33:44 -0400

    Test

--- Main.hs
+++ Main.hs
@@ -1,4 +1,10 @@
-import Tracker
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Tracker as T
 
 main = do
+    Just t <- T.open
+    T.echo t "asdf" >>= print
     print "hello world"
+
+    T.close t
