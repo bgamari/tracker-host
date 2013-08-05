bf398e742938f8eb0a7f5f3c349f9b5d7fb343ce
Ben Gamari <bgamari.foss@gmail.com>
2013-08-05 11:57:29 -0400

    Main: Factor out sample parsing

--- Main.hs
+++ Main.hs
@@ -4,11 +4,18 @@ import qualified Tracker as T
 import Control.Monad
 import Data.Binary.Get
 import Data.Int       
+       
+import qualified Data.Vector.Unboxed as V
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 
 getInt16le :: Get Int16
 getInt16le = fromIntegral `fmap` getWord16le
 
+parseSamples :: BS.ByteString -> V.Vector Int16
+parseSamples a = 
+    runGet (V.replicateM (BS.length a `div` 2) $ getInt16le) $ BSL.fromStrict a
+    
 main = do
     Just t <- T.open
     T.echo t "Hello World!" >>= print
@@ -16,6 +23,6 @@ main = do
     T.setFeedbackFreq t 1000
     T.setAdcFreq t 100
     T.startAdcStream t
-    forever $ do a <- T.readData t
-                 print $ runGet (replicateM 128 $ getInt16le) $ BSL.fromStrict a
+    forever $ do samples <- parseSamples `fmap` T.readData t
+                 print samples
     T.close t
