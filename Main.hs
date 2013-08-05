266cb0cc2c6872748a7edfbaf049515a2306c578
Ben Gamari <bgamari.foss@gmail.com>
2013-08-05 11:51:24 -0400

    ADC samples are signed

--- Main.hs
+++ Main.hs
@@ -3,7 +3,11 @@
 import qualified Tracker as T
 import Control.Monad
 import Data.Binary.Get
+import Data.Int       
 import qualified Data.ByteString.Lazy as BSL
+
+getInt16le :: Get Int16
+getInt16le = fromIntegral `fmap` getWord16le
 
 main = do
     Just t <- T.open
@@ -13,5 +17,5 @@ main = do
     T.setAdcFreq t 100
     T.startAdcStream t
     forever $ do a <- T.readData t
-                 print $ runGet (replicateM 127 $ getWord16le) $ BSL.fromStrict a
+                 print $ runGet (replicateM 127 $ getInt16le) $ BSL.fromStrict a
     T.close t
