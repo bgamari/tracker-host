7638c30b5cb37b62816471f977b090ccb31cb5a0
Ben Gamari <bgamari.foss@gmail.com>
2013-08-05 22:56:29 -0400

    Start implementing command line interface

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Maybe
 import Control.Monad
+import Control.Monad.IO.Class
 import Control.Applicative
 import Data.Binary.Get
 import Data.Int       
@@ -10,18 +12,41 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
        
 import qualified Tracker as T
+import Tracker (Stage(..), Psd(..))
 import Linear
+import System.Console.Haskeline
  
 roughScan :: T.RasterScan
 roughScan =
     maybe (error "Invalid scan") id
     $ T.scanAround (pure 0x7fff) (pure 0x1000) (V3 20 20 2)
 
-main = do
-    Just t <- T.open
-    T.echo t "Hello World!" >>= print
-    --T.setStageGains $ 
-    T.setFeedbackFreq t 1000
-    T.setAdcFreq t 5000
-    T.roughScan t 1000 roughScan >>= V.mapM_ (putStrLn . show . fst)
-    T.close t
+unitStageGains :: Stage (Stage Int32)
+unitStageGains = Stage (Stage 1 0 0) (Stage 0 1 0) (Stage 0 0 1)
+
+main :: IO ()    
+main = runInputT defaultSettings $ do
+    t <- fromJust (error "Couldn't open Tracker") <$> liftIO T.open
+    liftIO $ do T.echo t "Hello World!" >>= print
+                T.setStageGains t unitStageGains
+                T.setFeedbackFreq t 1000
+                T.setAdcFreq t 5000
+                --T.roughScan t 1000 roughScan >>= V.mapM_ (putStrLn . show . fst)
+    while $ prompt t
+    liftIO $ T.close t
+    
+while :: Monad m => m Bool -> m ()
+while m = m >>= \a->when a (while m)
+
+--prompt :: Tracker -> InputT IO ()
+prompt t = do
+    line <- getInputLine "> "
+    case maybe ["exit"] words line of
+      "exit":_                                      -> return False
+      cmd:rest | Just action <- lookup cmd commands -> action rest >> return True
+               | otherwise                          -> do outputStrLn $ "Unknown command: "++cmd
+                                                          return True
+      _                                             -> return True
+
+commands :: [(String, [String] -> InputT IO ())]
+commands = [ ("hello", const $ outputStrLn "hello") ]
