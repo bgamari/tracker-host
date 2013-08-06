30a42d04c6cd07a0c36bb91c568a69c24857cffc
Ben Gamari <bgamari.foss@gmail.com>
2013-08-06 13:05:26 -0400

    Move TrackerUI types into TrackerUI.Types

--- Main.hs
+++ Main.hs
@@ -1,43 +1,24 @@
-{-# LANGUAGE OverloadedStrings, PatternGuards, GeneralizedNewtypeDeriving, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, PatternGuards #-}
 
 import Data.Maybe
 import Control.Monad
 import Control.Monad.Trans.Class
-import Control.Monad.State
 import Control.Monad.IO.Class
 import Control.Applicative
-import Data.Binary.Get
-import Data.Int       
-       
-import qualified Data.Vector as V
+import Data.Int
+
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
-       
-import qualified Tracker as T
-import Tracker (TrackerT, Stage(..), Psd(..), Sensors, Sample)
+import qualified Data.Vector as V
+
 import Linear
 import System.Console.Haskeline
-import Control.Lens
- 
-data TrackerState
-    = TrackerState { _lastRoughCal :: Maybe (V.Vector (Sensors Sample))
-                   }
-makeLenses ''TrackerState
+import Data.Binary.Get
+import Control.Lens       
 
-newtype TrackerUI a = TUI (StateT TrackerState (InputT (TrackerT IO)) a)
-                    deriving ( Functor, Applicative, Monad, MonadIO
-                             , MonadState TrackerState )
-        
-liftInputT :: InputT (TrackerT IO) a -> TrackerUI a
-liftInputT = TUI . lift
-
-liftTracker :: TrackerT IO a -> TrackerUI a
-liftTracker = TUI . lift . lift
-
-runTrackerUI :: TrackerUI a -> IO (Either String a)
-runTrackerUI (TUI a) = T.withTracker $ runInputT defaultSettings $ evalStateT a state0
-  where state0 = TrackerState { _lastRoughCal = Nothing
-                              }
+import qualified Tracker as T
+import Tracker (TrackerT, Stage(..), Psd(..), Sensors, Sample)
+import TrackerUI.Types
 
 roughScan :: T.RasterScan
 roughScan =
@@ -47,49 +28,35 @@ roughScan =
 unitStageGains :: Stage (Stage Int32)
 unitStageGains = Stage (Stage 1 0 0) (Stage 0 1 0) (Stage 0 0 1)
 
-main :: IO ()    
-main = either error (const $ return ()) =<< go
-  where go = runTrackerUI $ do
-          liftTracker $ do T.echo "Hello World!" >>= liftIO . print
-                           T.setStageGains unitStageGains
-                           T.setFeedbackFreq 1000
-                           T.setAdcFreq 5000
-          while $ prompt
-    
-while :: Monad m => m Bool -> m ()
-while m = m >>= \a->when a (while m)
+command :: String -> String -> String -> ([String] -> TrackerUI ()) -> Command
+command name help args action = Cmd name help args (\a->action a >> return True)
 
-prompt :: TrackerUI Bool
-prompt = do
-    line <- liftInputT $ getInputLine "> "
-    case maybe ["exit"] words line of
-      "exit":_                                 -> return False
-      "quit":_                                 -> return False
-      cmd:rest | Just action <- lookupCmd cmd  -> action rest >> return True
-               | otherwise                     -> do
-                   liftInputT $ outputStrLn $ "Unknown command: "++cmd
-                   return True
-      _         -> return True
-  where lookupCmd :: String -> Maybe ([String] -> TrackerUI ())
-        lookupCmd cmd = lookup cmd $ map (\(c,_,a)->(c,a)) commands
-
-type Command = (String, String, [String] -> TrackerUI ())
-
-command :: String -> String -> ([String] -> TrackerUI ()) -> Command
-command cmd help action = (cmd, help, action)
+exitCmd :: Command
+exitCmd = Cmd "exit" "Exit the program" "" $ const $ return False
 
 helloCmd :: Command
-helloCmd = command "hello" help $ const $ liftInputT $ outputStrLn "hello world!"
+helloCmd = command "hello" help ""
+    $ const $ liftInputT $ outputStrLn "hello world!"
   where help = "Print hello world!"
-    
+
 roughCalCmd :: Command
-roughCalCmd = command "rough-cal" help $ \args->do
+roughCalCmd = command "rough-cal" help "" $ \args->do
     scan <- liftTracker $ T.roughScan 1000 roughScan
     lastRoughCal .= Just scan
   where help = "Perform rough calibration"
-    
+
 dumpRoughCmd :: Command
-dumpRoughCmd = command "dump-rough" help $ \args->do
+dumpRoughCmd = command "dump-rough" help "FILENAME" $ \args->do
+    let fname = fromMaybe "rough-cal.txt" $ listToMaybe args
+    scan <- use lastRoughCal
+    case scan of
+        Nothing -> liftInputT $ outputStrLn "No rough calibration done."
+        Just s  -> liftIO $ writeFile fname
+                          $ unlines $ map (show . T.stage) $ V.toList s
+  where help = "Dump last rough calibration"
+
+helpCmd :: Command
+helpCmd = command "help" help "[CMD]" $ \args->do
     let fname = fromMaybe "rough-cal.txt" $ listToMaybe args
     scan <- use lastRoughCal
     case scan of
@@ -102,4 +69,29 @@ commands :: [Command]
 commands = [ helloCmd
            , roughCalCmd
            , dumpRoughCmd
+           , exitCmd
            ]
+
+prompt :: TrackerUI Bool
+prompt = do
+    line <- liftInputT $ getInputLine "> "
+    case maybe ["exit"] words line of
+      cmd:rest | Just c <- lookupCmd cmd  -> c^.cmdAction $ rest
+               | otherwise                -> do
+                   liftInputT $ outputStrLn $ "Unknown command: "++cmd
+                   return True
+      _ | otherwise                       -> return True
+  where lookupCmd :: String -> Maybe Command
+        lookupCmd cmd = lookup cmd $ map (\c->(c^.cmdName, c)) commands
+
+main :: IO ()
+main = either error (const $ return ()) =<< go
+  where go = runTrackerUI $ do
+          liftTracker $ do T.echo "Hello World!" >>= liftIO . print
+                           T.setStageGains unitStageGains
+                           T.setFeedbackFreq 1000
+                           T.setAdcFreq 5000
+          while $ prompt
+
+while :: Monad m => m Bool -> m ()
+while m = m >>= \a->when a (while m)
---
+++ TrackerUI/Types.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, TemplateHaskell #-}
+
+module TrackerUI.Types where
+
+import Control.Monad.State
+import Control.Applicative
+
+import qualified Data.Vector as V
+import System.Console.Haskeline
+import Control.Lens
+
+import qualified Tracker as T
+import Tracker (TrackerT, Stage(..), Psd(..), Sensors, Sample)
+
+data TrackerState
+    = TrackerState { _lastRoughCal :: Maybe (V.Vector (Sensors Sample))
+                   }
+makeLenses ''TrackerState
+           
+defaultTrackerState :: TrackerState           
+defaultTrackerState =
+    TrackerState { _lastRoughCal = Nothing
+                 }
+
+newtype TrackerUI a = TUI (StateT TrackerState (InputT (TrackerT IO)) a)
+                    deriving ( Functor, Applicative, Monad, MonadIO
+                             , MonadState TrackerState )
+
+liftInputT :: InputT (TrackerT IO) a -> TrackerUI a
+liftInputT = TUI . lift
+
+liftTracker :: TrackerT IO a -> TrackerUI a
+liftTracker = TUI . lift . lift
+
+runTrackerUI :: TrackerUI a -> IO (Either String a)
+runTrackerUI (TUI a) =
+    T.withTracker $ runInputT defaultSettings $ evalStateT a defaultTrackerState
+
+data Command = Cmd { _cmdName   :: String
+                   , _cmdHelp   :: String
+                   , _cmdArgs   :: String
+                   , _cmdAction :: [String] -> TrackerUI Bool
+                   }
+makeLenses ''Command
