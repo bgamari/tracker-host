a0ef25830e11bdab32af7783707c09aa9a796fbe
Ben Gamari <bgamari.foss@gmail.com>
2013-08-20 13:21:16 -0400

    Begin reworking sensor input handling

--- Tracker/Monad.hs
+++ Tracker/Monad.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving, TemplateHaskell #-}
 
 module Tracker.Monad ( TrackerT
                      , withDevice
                      , withDeviceIO
                      , runTrackerT
                      , liftThrough
+                       -- * Sensor sample notifications
+                     , SensorQueue
+                     , getSensorQueue
+                     , notifySensors
                        -- * Convenient re-exports
                      , MonadIO, liftIO
                      ) where
@@ -15,20 +19,56 @@ import Control.Applicative
 import System.Console.Haskeline.MonadException
 import System.USB
 
-newtype TrackerT m a = TrackerT (ReaderT DeviceHandle m a)
-                     deriving ( Functor, Applicative, Monad , MonadIO, MonadTrans )
+import Control.Concurrent.STM
+import Control.Concurrent.Async
 
-deriving instance MonadException m => MonadException (TrackerT m)
+import qualified Data.Vector as V
+import Data.Int
+import Data.Maybe
+import Control.Lens
+       
+import Tracker.Types
 
+type SensorQueue = TChan (V.Vector (Sensors Int16))
+       
+data Env = Env { _device         :: DeviceHandle
+               , _sensorThread   :: Async ()
+               , _sensorQueue    :: SensorQueue
+               }
+makeLenses ''Env
+     
+newtype TrackerT m a = TrackerT (ReaderT Env m a)
+                     deriving ( Functor, Applicative, Monad , MonadIO, MonadException )
+        
+instance MonadTrans TrackerT where
+    lift = TrackerT . lift
+
+getSensorQueue :: MonadIO m => TrackerT m SensorQueue
+getSensorQueue = TrackerT (view sensorQueue) >>= liftIO . atomically . dupTChan
+
+notifySensors :: MonadIO m => V.Vector (Sensors Int16) -> TrackerT m ()
+notifySensors sensors = TrackerT $ do
+    queue <- view sensorQueue
+    liftIO $ atomically $ writeTChan queue sensors
+    
 withDevice :: Monad m => (DeviceHandle -> m a) -> TrackerT m a
-withDevice f = TrackerT $ ask >>= lift . f
+withDevice f = TrackerT (view device) >>= lift . f
 
 withDeviceIO :: MonadIO m => (DeviceHandle -> IO a) -> TrackerT m a
-withDeviceIO f = TrackerT ask >>= liftIO . f
+withDeviceIO f = TrackerT (view device) >>= liftIO . f
 
-runTrackerT :: TrackerT m a -> DeviceHandle -> m a
-runTrackerT (TrackerT m) h = runReaderT m h
+sensorListen :: SensorQueue -> IO ()
+sensorListen = undefined
+
+runTrackerT :: MonadIO m => TrackerT m a -> DeviceHandle -> m a
+runTrackerT (TrackerT m) h = do
+    queue <- liftIO $ newBroadcastTChanIO
+    thread <- liftIO $ async $ sensorListen queue
+    let env = Env h thread queue
+    runReaderT m env
             
 -- | This is required for async which is monomorphic in IO            
 liftThrough :: MonadIO m => (IO a -> IO b) -> TrackerT IO a -> TrackerT m b
-liftThrough f (TrackerT a) = withDeviceIO $ f . runReaderT a
+liftThrough f (TrackerT a) = TrackerT $ do
+    r <- ask
+    liftIO $ f $ runReaderT a r
