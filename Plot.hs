6d0817f042a6bc98ee9985f5fc29c7e05bfed3b3
Ben Gamari <bgamari.foss@gmail.com>
2013-08-21 20:54:18 -0400

    Reimplement plotting on top of gl-plot

--- Plot.hs
+++ Plot.hs
@@ -1,53 +1,46 @@
 module Plot (startPlot) where
       
-import Data.Foldable
-import Graphics.Gloss
-import Graphics.Gloss.Interface.IO.Animate
-import Linear
+import Data.Traversable as T
 import Data.Int
 import Control.Monad
+import Control.Applicative
 import Control.Concurrent
 import Control.Concurrent.STM
 import qualified Data.Vector as V
+import qualified Data.Vector.Storable as VS
 import Control.Lens
+
+import Linear
 import Tracker 
 import TrackerUI.Types
 
-npoints = 1000
-       
-points :: (Functor f, Foldable f) => f (V2 Float) -> Picture
-points = pictures . toList . fmap (\(V2 x y)->translate x y $ circle 0.5)
+import Graphics.Rendering.GLPlot
+import Graphics.UI.GLUT as GLUT
 
-plot :: (Functor f, Foldable f) => [(Color, f (V2 Float))] -> Picture       
-plot curves =
-    pictures $ [ color black $ line [(0xffff,0), (0,0), (0,0xffff)] ]
-            ++ map (\(c,x)->color c $ points x) curves
+npoints = 4000
 
-fixPoints :: Real a => V.Vector a -> V.Vector (V2 Float)
-fixPoints = fmap (\(x,y)->V2 (realToFrac x) (realToFrac y)) . V.indexed
+fixPoints :: (VS.Storable a, Real a) => VS.Vector a -> VS.Vector (V2 GLfloat)
+fixPoints = VS.imap (\x y->V2 (realToFrac x) (realToFrac y))
 
 decimate :: Int -> V.Vector a -> V.Vector a
 decimate n = fmap snd . V.filter (\(i,_)->i `mod` n == 0) . V.indexed
 
-update :: TVar (V.Vector (Sensors Int16)) -> Float -> IO Picture
-update ptsVar t = do
-    let n = 4
-    pts <- atomically $ readTVar ptsVar
-    return $ plot
-        [ (blue,   fixPoints $ decimate n $ fmap (\a->a^.psd._x.sdDiff) pts)
-        , (red,    fixPoints $ decimate n $ fmap (\a->a^.psd._x.sdSum) pts)
-        , (green,  fixPoints $ decimate n $ fmap (\a->a^.psd._y.sdDiff) pts)
-        , (yellow, fixPoints $ decimate n $ fmap (\a->a^.psd._y.sdSum) pts)
-        ]
+curves :: Sensors (VS.Vector Int16) -> [Curve]
+curves pts =
+    [Curve (Color4 0 0 0 0) (fixPoints $ pts ^. psd ^. _x ^. sdDiff)]
 
 plotWorker :: Int -> TChan (V.Vector (Sensors Int16)) -> IO ()
 plotWorker npoints queue = do
-    points <- newTVarIO V.empty
-    forkIO $ forever $ do
-        v <- atomically $ readTChan queue
-        atomically $ do old <- readTVar points
-                        writeTVar points $ V.take npoints $ v V.++ old
-    animateIO (InWindow "Tracker" (300,300) (400,400)) white (update points)
+    GLUT.getArgsAndInitialize
+    plot <- newPlot "Tracker"
+    let go :: Sensors (VS.Vector Int16) -> IO ()
+        go v = do
+            new <- atomically $ readTChan queue
+            let v' = fmap (VS.take npoints) $ (VS.++) <$> fmap VS.convert (T.sequenceA new) <*> v
+            updateCurves plot $ curves v'
+            go v'
+    forkIO $ go (pure VS.empty)
+    GLUT.mainLoop
 
 startPlot :: MonadIO m => TrackerT m ()
 startPlot = do
