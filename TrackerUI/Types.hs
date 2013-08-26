89a89cb518c74d0aabce78638d851f76a56c553f
Ben Gamari <bgamari.foss@gmail.com>
2013-08-26 16:09:18 -0400

    Lower default rough-cal scan size

--- TrackerUI/Types.hs
+++ TrackerUI/Types.hs
@@ -40,7 +40,7 @@ defaultTrackerState =
     TrackerState { _lastRoughCal  = Nothing
                  , _roughScanFreq = 1000
                  , _roughScan     = RasterScan { _scanCenter = pure 0x7fff
-                                               , _scanSize   = pure 0x1000
+                                               , _scanSize   = pure 0x400
                                                , _scanPoints = Stage $ V3 20 20 2
                                                }
                  , _fineScan      = FineScan { _fineScanRange  = pure 0x500
