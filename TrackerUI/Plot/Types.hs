f1be2c46eb4ad69ac84554c6af9dd44a87a51981
Ben Gamari <bgamari.foss@gmail.com>
2013-09-03 13:50:47 -0400

    TrackerUI: Add missing Plot.Types

---
+++ TrackerUI/Plot/Types.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+                
+module TrackerUI.Plot.Types where
+
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Lens
+import Data.Int
+
+data PlotConfig = PlotConfig { _pcYSize    :: Maybe Int16
+                             , _pcNPoints  :: Int
+                             }
+                deriving (Show)
+makeLenses ''PlotConfig
+
+data TrackerPlot = TrackerPlot { _tpWorker   :: ThreadId
+                               , _tpConfig   :: TVar PlotConfig
+                               }
+makeLenses ''TrackerPlot
