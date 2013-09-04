4d6f4f4ce9cf50844c85227b9f63da224f604e79
Ben Gamari <bgamari.foss@gmail.com>
2013-09-03 20:21:46 -0400

    Types: Use Data.Fixed

--- Tracker/Types.hs
+++ Tracker/Types.hs
@@ -27,6 +27,7 @@ import Data.Foldable
 import Data.Traversable
 import Data.Fixed
 import Data.Binary
+import Data.Binary.Get (getWord32le)
 import Data.Binary.Put (putWord32le)
 import Control.Applicative       
 import Data.Distributive (Distributive)
@@ -37,39 +38,17 @@ import Control.Monad.IO.Class
 -- | An ADC sample       
 type Sample = Int16
 
+data F16 = F16
 -- | A signed 16.16 fixed-point number
-newtype Fixed16 = Fixed16 Int32
-                deriving (Show, Read, Eq, Ord)
+newtype Fixed16 = Fixed16 (Fixed F16)
+                deriving (Show, Read, Eq, Ord, Fractional, Num, Real, RealFrac)
                 
-instance HasResolution Fixed16 where
-    resolution _ = 0xffff
+instance HasResolution F16 where
+    resolution _ = 0x10000
     
-instance Num Fixed16 where
-    Fixed16 a + Fixed16 b = Fixed16 (a + b)
-    Fixed16 a * Fixed16 b = Fixed16 (a * b `div` 0x10000)
-    negate (Fixed16 a)    = Fixed16 (-a)
-    abs (Fixed16 a)
-      | a <  0    = Fixed16 (-a)
-      | otherwise = Fixed16 a
-    signum (Fixed16 a)
-      | a == 0    =  0
-      | a <  0    = -1
-      | a >  0    =  1
-    fromInteger n = Fixed16 $ fromIntegral $ 0x10000 * n
-    
-instance Real Fixed16 where
-    toRational (Fixed16 a) = fromIntegral a % 0x10000
-    
-instance Fractional Fixed16 where
-    Fixed16 a / Fixed16 b = Fixed16 (0x10000 * a `div` b)
-    recip a               = fromIntegral 1 / a
-    fromRational a        = Fixed16 $ fromIntegral $ 0x10000 * num `div` denom
-      where num = numerator a
-            denom = denominator a
-
 instance Binary Fixed16 where
-    get = Fixed16 <$> get
-    put (Fixed16 a) = putWord32le $ fromIntegral a
+    get = Fixed16 . fromIntegral <$> getWord32le
+    put (Fixed16 a) = putWord32le $ round $ 0x10000 * a
     
 -- | The stage coordinate frame
 newtype Stage a = Stage {unStage :: V3 a}
