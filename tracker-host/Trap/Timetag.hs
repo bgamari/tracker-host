15b946fe86093aceb9d1524a976c10189994b294
Ben Gamari <bgamari.foss@gmail.com>
2014-10-23 15:12:42 -0400

    Timetag: Tighten things up

--- tracker-host/Trap/Timetag.hs
+++ tracker-host/Trap/Timetag.hs
@@ -3,12 +3,16 @@
 {-# LANGUAGE BangPatterns #-}
 
 module Trap.Timetag
-    ( Timetag
+    ( -- * Opening a connection
+      Timetag
     , open
+    , close
+      -- * Managing capture state
     , startCapture
     , stopCapture
     , resetCounter
     , isCaptureRunning
+      -- * Managing capture outputs
     , OutputId
     , addOutputFd
     , removeOutput
@@ -19,22 +23,28 @@ import Control.Monad (void, when)
 import Control.Monad.IO.Class
 import Data.Monoid
 import qualified Data.ByteString.Char8 as BS
-import Control.Concurrent (threadDelay)
 
-import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket hiding (send, sendTo, recv, recvFrom, close)
+import qualified Network.Socket as Socket
 import Network.Socket.ByteString
 import System.Posix.Types
 
 newtype Timetag = Timetag Socket
 
+tryIO' :: MonadIO m => IO a -> EitherT String m a
+tryIO' = fmapLT show . tryIO
+
 open :: MonadIO m => FilePath -> EitherT String m Timetag
 open path = do
-    s <- liftIO $ socket AF_UNIX Stream defaultProtocol
-    liftIO $ connect s (SockAddrUnix path)
-    ready <- liftIO $ recvUntil '\n' s
+    s <- tryIO' $ socket AF_UNIX Stream defaultProtocol
+    tryIO' $ connect s (SockAddrUnix path)
+    ready <- tryIO' $ recvUntil '\n' s
     if ready `BS.isPrefixOf` ready
       then return $ Timetag s
       else left $ "Expected 'ready', saw "++show ready
+
+close :: MonadIO m => Timetag -> EitherT String m ()
+close (Timetag s) = tryIO' $ Socket.close s
 
 recvUntil :: Char -> Socket -> IO BS.ByteString
 recvUntil term s = go BS.empty
@@ -43,19 +53,19 @@ recvUntil term s = go BS.empty
         c <- liftIO $ recv s 1
         case () of
           () | BS.null c         -> return accum
-             |BS.head c == term  -> return accum
+             | BS.head c == term -> return accum
              | otherwise         -> go (accum <> c)
 
 command :: BS.ByteString -> Timetag -> EitherT String IO (Maybe BS.ByteString)
 command cmd tt@(Timetag s) = do
-    n <- liftIO $ send s cmd
+    n <- tryIO' $ send s cmd
     readReply tt
 
 readReply :: Timetag -> EitherT String IO (Maybe BS.ByteString)
 readReply (Timetag s) = go BS.empty
   where
     go !accum = do
-      reply <- liftIO $ recvUntil '\n' s
+      reply <- tryIO' $ recvUntil '\n' s
       case () of
         _ | BS.null reply                 ->
             left "Timetag.readReply: timetag_acquire connection terminated"
@@ -64,7 +74,7 @@ readReply (Timetag s) = go BS.empty
         _ | "ready" `BS.isPrefixOf` reply -> do
              return $ if BS.null accum then Nothing else Just accum
         _ | "error" `BS.isPrefixOf` reply -> do
-            ready <- liftIO $ recvUntil '\n' s -- kill next "ready"
+            ready <- tryIO' $ recvUntil '\n' s -- kill next "ready"
             when (not $ "ready" `BS.isPrefixOf` ready)
                 $ left $ "Expected 'ready' after 'error', saw "++show ready
             left $ "Timetag.readReply: error reply: "++show reply
@@ -88,9 +98,9 @@ addOutputFd :: Timetag
             -> EitherT String IO OutputId
 addOutputFd tt@(Timetag s) name (Fd fd) = do
     let cmd = "add_output_fd "<>name<>"\n"
-    liftIO $ send s cmd
-    reply <- readReply tt
-    liftIO $ sendFd s fd
+    tryIO' $ send s cmd
+    _ <- readReply tt
+    tryIO' $ sendFd s fd
     reply <- readReply tt
     case reply of
       Just reply'
@@ -112,4 +122,5 @@ isCaptureRunning tt = do
       Just s
         | s == "1" -> return True
         | s == "0" -> return False
+        | otherwise-> left $ "invalid reply "++show s
       Nothing      -> left "expected reply"
